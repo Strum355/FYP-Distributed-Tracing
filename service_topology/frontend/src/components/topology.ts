582132004f53a55e3bc2d3edb297ec45a74bebb6
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2019-10-04 16:29:22 +0100

    Single quotes only in this house

--- service_topology/frontend/src/components/topology.ts
+++ service_topology/frontend/src/components/topology.ts
@@ -1,22 +1,23 @@
-import ForceGraph from 'force-graph';
-import { Component, Vue } from 'vue-property-decorator';
+import ForceGraph from 'force-graph'
+import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class Topology extends Vue {
   public mounted() {
-    let node0: ForceGraph.GraphNode = {}
-    let node1: ForceGraph.GraphNode = {id: "Myriel", name: "burger", val: ""}
-    let node2: ForceGraph.GraphNode = {id: "Blop", name: "burger", val: ""}
+    let node1: ForceGraph.GraphNode = {id: 'Myriel', name: 'burger', val: ''}
+    let node2: ForceGraph.GraphNode = {id: 'Blop', name: 'burger', val: ''}
 
     let data: ForceGraph.GraphData = {
       nodes: [
         node1, node2,
       ],
       links: [
-        {source: node1, target: node2, type: "bepis"},
+        {source: node1, target: node2, type: 'bepis'},
       ]
     }
 
-    ForceGraph()(this.$el as HTMLElement).graphData(data)
+    ForceGraph()(this.$el as HTMLElement).graphData(data).onNodeClick((node: ForceGraph.GraphNode) => {
+      console.log(node.id)
+    })
   }
 }

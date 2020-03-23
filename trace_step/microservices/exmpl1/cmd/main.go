44313aeacd2fa96601600a6a0191a4bc11f86adb
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2020-03-23 02:06:55 +0000

    OpenTracing exampls and more OpenTelemetry info

--- trace_step/microservices/exmpl1/cmd/main.go
+++ trace_step/microservices/exmpl1/cmd/main.go
@@ -8,7 +8,7 @@ import (
 	tracestep "github.com/Strum355/FYP-2020_Distributed-Tracing/trace_step/golang"
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
-	"github.com/uber/jaeger-client-go/config"
+	"github.com/uber/jaeger-client-go/transport"
 )
 
 const (
@@ -17,12 +17,10 @@ const (
 )
 
 func main() {
-	rep, _ := (&config.ReporterConfig{LocalAgentHostPort: "localhost:6831"}).NewReporter(serviceName, nil, nil)
-	sampler, _ := (&config.SamplerConfig{Type: "const", Param: 1.0}).NewSampler(serviceName, nil)
-	tracer1, closer1 := jaeger.NewTracer(serviceName, sampler, rep, jaeger.TracerOptions.MaxTagValueLength(stackLength))
-	defer closer1.Close()
+	tracer, closer := jaeger.NewTracer(serviceName, jaeger.NewConstSampler(true), jaeger.NewRemoteReporter(transport.NewHTTPTransport("localhost:14268")), jaeger.TracerOptions.MaxTagValueLength(stackLength))
+	defer closer.Close()
 
-	opentracing.SetGlobalTracer(tracestep.NewTracerWrapper(tracer1))
+	opentracing.SetGlobalTracer(tracestep.NewTracerWrapper(tracer))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		span := opentracing.StartSpan("start-req", tracestep.WithCallstackOffset(1))

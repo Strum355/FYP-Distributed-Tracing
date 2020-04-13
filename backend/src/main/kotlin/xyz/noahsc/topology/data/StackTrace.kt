2a52031e46b6a879a662430aff5f36cf1a46f03d
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2020-04-13 16:27:29 +0100

    made span tag parsing nicer for stacktrace collecting

--- backend/src/main/kotlin/xyz/noahsc/topology/data/StackTrace.kt
+++ backend/src/main/kotlin/xyz/noahsc/topology/data/StackTrace.kt
@@ -5,20 +5,14 @@ import xyz.noahsc.topology.data.stackparser.*
 data class StackTrace(val stackFrames: List<StackFrame>) {
     companion object {
         fun fromSpan(span: Span): StackTrace {
-            val tag = span.tags?.filter { it.key == "_tracestep_lang" }?.get(0)
-            val stack = span.tags?.filter { it.key == "_tracestep_stack" }?.get(0)
-            return when(tag?.value) {
-                "go" -> GolangStackParser(stack!!.value, 
-                    span.tags.filter { 
-                        it.key == "_tracestep_execpath" 
-                    }.get(0).value
-                ).parse()
-                "nodejs" -> NodeJSStackParser(stack!!.value,
-                    span.tags.filter { 
-                        it.key == "_tracestep_execpath" 
-                    }.get(0).value
-                ).parse()
-                else -> StackTrace(listOf())
+            val tags = span.tags ?: return StackTrace(emptyList())
+            val tag = tags.filter { it.key == "_tracestep_lang" }.get(0).value
+            val stack = tags.filter { it.key == "_tracestep_stack" }.get(0).value
+            val execpath = span.tags.filter { it.key == "_tracestep_execpath" }.get(0).value
+            return when(tag) {
+                "go" -> GolangStackParser(stack, execpath).parse()
+                "nodejs" -> NodeJSStackParser(stack, execpath).parse()
+                else -> StackTrace(emptyList())
             }
         }
     }

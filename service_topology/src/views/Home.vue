f6b1bc4ce941146d28824a8795e18889518af027
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2020-04-15 22:17:52 +0100

    Removed incomplete service toplogy source

--- service_topology/src/views/Home.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div id="home">
-        <TopologyGraph/>
-    </div>
-</template>
-
-<script lang="ts" src="./home.ts"></script>
--- service_topology/src/views/home.ts
+++
@@ -1,9 +0,0 @@
-import TopologyGraph from '@/components/TopologyGraph.vue'
-import { Component, Vue } from 'vue-property-decorator'
-
-@Component({
-  components: {
-    TopologyGraph,
-  },
-})
-export default class Home extends Vue {}

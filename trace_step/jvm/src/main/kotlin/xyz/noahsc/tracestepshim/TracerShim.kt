891cb6821fdedcf9b94d4bd8e56f07e750396652
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2020-04-17 07:31:39 +0100

    reading jvm Main-Class from manifest file

--- trace_step/jvm/src/main/kotlin/xyz/noahsc/tracestepshim/TracerShim.kt
+++ trace_step/jvm/src/main/kotlin/xyz/noahsc/tracestepshim/TracerShim.kt
@@ -6,59 +6,69 @@ import io.opentracing.tag.Tag
 import io.opentracing.SpanContext
 import java.lang.StackTraceElement
 
-public class TracerShim(val tracer: Tracer, val stackOffset: Int, val packageRoot: String): Tracer by tracer {
-    override fun buildSpan(var1: String): Tracer.SpanBuilder {
-        println("building span")
-        return SpanBuilder(tracer.buildSpan(var1), packageRoot);
-    }
-}
+private val mainClassRe = Regex("""Main-Class: ((?:[a-zA-Z]+\.)+)[a-zA-Z]+""")
 
-private class SpanBuilder(var builder: Tracer.SpanBuilder, val packageRoot: String): Tracer.SpanBuilder{
-    override fun start(): Span {
-        println("hello world")
-        val span = builder.start();
-        val stackTrace = Throwable().stackTrace.filter { it ->
-            it.className.startsWith(packageRoot)
-        }.forEach { println(it) }
-        
-        span.setTag("_tracestep_lang", "jvm")
-        span.setTag("_tracestep_stack", "asdf")
-        return span
+public class TracerShim(val tracer: Tracer, val stackOffset: Int): Tracer by tracer {
+    val packageRoot: String
+    init {
+        val manifest = this.javaClass.classLoader.getResource("META-INF/MANIFEST.MF")
+        packageRoot = mainClassRe.find(manifest.readText())!!.destructured.component1().removeSuffix(".")
+        println(packageRoot)
     }
 
-    override fun asChildOf(parent: SpanContext) = apply {
-        builder = builder.asChildOf(parent)
+    override fun buildSpan(operationName: String): Tracer.SpanBuilder {
+        return SpanBuilder(tracer.buildSpan(operationName), packageRoot)
     }
 
-    override fun ignoreActiveSpan() = apply {
-        builder = builder.ignoreActiveSpan()
-    }
-
-    override fun asChildOf(parent: Span) = apply {
-        builder = builder.asChildOf(parent)
-    }
-
-    override fun addReference(referenceType: String, referencedContext: SpanContext) = apply {
-        builder = builder.addReference(referenceType, referencedContext)
-    }
-
-    override fun withTag(key: String, value: String) = apply {
-        builder = builder.withTag(key, value)
-    }
-
-    override fun <T>withTag(tag: Tag<T>, value: T) = apply {
-        builder = builder.withTag(tag, value)
-    }
-
-    override fun withTag(key: String, value: Boolean) = apply {
-        builder = builder.withTag(key, value)
-    }
-
-    override fun withTag(key: String, value: Number) = apply {
-        builder = builder.withTag(key, value)
-    }
-
-    override fun withStartTimestamp(microseconds: Long) = apply {
-        builder = builder.withStartTimestamp(microseconds)
+    private class SpanBuilder(var builder: Tracer.SpanBuilder, val packageRoot: String): Tracer.SpanBuilder {
+        override fun start(): Span {
+            val span = builder.start()
+            val stackBuilder = StringBuilder()
+            Thread.currentThread().stackTrace.filter { it ->
+                it.className.startsWith(packageRoot)
+            }.forEach { 
+                stackBuilder.append("${it.className} ${it.lineNumber}\n")
+            }
+            
+            span.setTag("_tracestep_lang", "jvm")
+            span.setTag("_tracestep_stack", stackBuilder.toString())
+            return span
+        }
+    
+        override fun asChildOf(parent: SpanContext) = apply {
+            builder = builder.asChildOf(parent)
+        }
+    
+        override fun ignoreActiveSpan() = apply {
+            builder = builder.ignoreActiveSpan()
+        }
+    
+        override fun asChildOf(parent: Span) = apply {
+            builder = builder.asChildOf(parent)
+        }
+    
+        override fun addReference(referenceType: String, referencedContext: SpanContext) = apply {
+            builder = builder.addReference(referenceType, referencedContext)
+        }
+    
+        override fun withTag(key: String, value: String) = apply {
+            builder = builder.withTag(key, value)
+        }
+    
+        override fun <T>withTag(tag: Tag<T>, value: T) = apply {
+            builder = builder.withTag(tag, value)
+        }
+    
+        override fun withTag(key: String, value: Boolean) = apply {
+            builder = builder.withTag(key, value)
+        }
+    
+        override fun withTag(key: String, value: Number) = apply {
+            builder = builder.withTag(key, value)
+        }
+    
+        override fun withStartTimestamp(microseconds: Long) = apply {
+            builder = builder.withStartTimestamp(microseconds)
+        }
     }
 }
